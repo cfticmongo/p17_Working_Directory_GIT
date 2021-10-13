de3045728317863e660ea35da5ebc5c8ef17945a
cfticmongo <cfticmongo@gmail.com>
2021-10-13 19:29:33 +0200

    severals

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get()
+app.get('/', (req, res) => {
+    // código
+})
 
 app.listen(3000, ()=>{
     console.log('Servidor escuchando en http://localhost:3000');
