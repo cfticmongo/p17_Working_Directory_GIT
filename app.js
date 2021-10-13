4781b6951e4fc31426c0ba9a9bdc3cf76796c0e4
cfticmongo <cfticmongo@gmail.com>
2021-10-13 20:50:36 +0200

    log import

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const Customer = require('./models/customer');
+const Log = require('./middleware/log');
 
 app.post('/', () => {
     Customer.getCustomer();
