af96d80ebeff9d8a7ac697ff82256d86140a3d8b
cfticmongo <cfticmongo@gmail.com>
2021-10-14 20:41:57 +0200

    add pedidos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const Customer = require('./models/customer');
+const pedidos = require('./models/pedido');
 const Log = require('./middleware/log');
 const cors = require('cors');
 
