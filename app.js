2dac53645238c833579295b35ae5d1c543a5868a
cfticmongo <cfticmongo@gmail.com>
2021-10-14 20:19:01 +0200

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 const Customer = require('./models/customer');
 const Log = require('./middleware/log');
+const cors = require('cors');
+
+app.use(cors());
 
 app.post('/', () => {
     Customer.getCustomer();
